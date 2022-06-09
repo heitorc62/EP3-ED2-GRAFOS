7116228701373fb8367b8ee9e496be84673f8a45
heitorc62 <heitor.c@usp.br>
2022-06-09 13:04:59 -0300

    Inicio da construção da classe que representará um grafo. Mudança na ideia da estrutura de dados que representará o grafo, antes: Dict de listas ligadas. Agora: Dict de vectors.

---
+++ codigo/Grafo.h
@@ -0,0 +1,29 @@
+#ifndef GRAFO_H
+#define GRAFO_H
+#include <iostream>
+#include <map>
+
+
+using namespace std;
+
+template<class Item>
+class Node{
+    public:
+    Node(){};
+    Node * next;
+    Item conteudo; //Conteúdo será o conteudo do vértice na lista de adjacências
+};
+
+template<class Item>
+class Grafo{
+    public:
+    Grafo(){};
+    Grafo()
+
+};
+
+
+
+
+
+#endif
